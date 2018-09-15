cd22e39cd675697b6619f3e10576a044fcd5d4eb
justinefawno <justinefawno@gmail.com>
2018-09-15 11:24:31 -0500

    Update Faculty.cs

--- ImplementingClassModels/Faculty.cs
+++ ImplementingClassModels/Faculty.cs
@@ -10,14 +10,14 @@ namespace ImplementingClassModels
     class Faculty : IFaculty
     {
         private string _name;
-
+        private List(Section) _sections;
         public Faculty(string n)
         {
             _name = n;
         }
         public void AssignSection(Section assignedSection)
         {
-            
+            _sections.add(assignedSection);
         }
 
         public string GetName()
